dd1e52fa0e6e457608331918284c8d348a47fb5c
Truong31 <lxtruong3107@gmail.com>
2025-06-05 19:16:02 +0700

    Update cho Camera Page, Laptop Page(Phần View All). Thêm trang Đăng nhập, đăng ký

--- frontend/src/pages/cameraPage/index.jsx
+++ frontend/src/pages/cameraPage/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import Header from "../../components/ui/Header";
 import Footer from "../../components/ui/Footer";
-import FeaturedProducts from "../laptopPage/components/FeaturedProducts";
+import FeaturedProducts from "./components/FeaturedProducts";
 
 const cameraFeaturedProducts = [
 	{
